c522be3801d4a2c61780de9491aae0cd44796381
RetroAspect <104701332+RetroAspect@users.noreply.github.com>
2022-04-30 19:07:50 -0500

    Create HttpRequestURL.java

---
+++ HttpRequestURL.java
@@ -0,0 +1,12 @@
+// Credits: Eric Pogue
+
+class HttpRequestURLs extends HttpRequest {
+    HttpRequestURLs() {
+        super();
+    }
+
+    public Boolean readURL(String urlInput) {
+        Boolean returnValue = super.readURL(urlInput);
+        return returnValue;
+    }
+}
